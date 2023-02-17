4ca43fdc5ebdd80512474956b43480134d722b04
trebektas <trensarbektas@gmail.com>
2023-02-17 13:31:31 +0100

    add nav.jsx to Exercise-Merge folder

---
+++ merge-app/Exercise-Merge/nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <header className="logoAndNavbar">
+      <nav className="nav"></nav>
+    </header>
+  );
+};
+
+export default Nav;

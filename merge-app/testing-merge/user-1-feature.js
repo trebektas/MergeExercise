e52943b2c49a342b2032b07204bff7b6d974dc68
trebektas <trensarbektas@gmail.com>
2023-02-17 13:10:33 +0100

    user-III commit

---
+++ merge-app/testing-merge/user-1-feature.js
@@ -0,0 +1,5 @@
+const userThree = () => {
+  return <p>This is user-III's function for user-I</p>;
+};
+
+export default userThree;
---
+++ merge-app/testing-merge/user-2-feature.js
@@ -0,0 +1,5 @@
+const userThree = () => {
+  return <p>This is user-III's function for user-II</p>;
+};
+
+export default userThree;
